ada7f3567fb35cade4d9888f0aacedb46a24ac90
Rebecca Brietzke <rebeccabrietzke@Rebeccas-MacBook-Pro.local>
2021-08-27 16:04:35 -0700

    refactored local storage code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import BgColor from './BgColor';
 
 function App() {
-	let randColorStored = localStorage.getItem('color');
-	const [color, setColor] = useState(randColorStored);
+	let getColor = localStorage.getItem('color');
+	const [color, setColor] = useState(getColor ? getColor : '2B6BE4');
 	function randomColor() {
 		let randNum = Math.floor(Math.random() * 16777215)
 			.toString(16)
 			.toUpperCase();
-
 		localStorage.setItem('color', randNum);
 		setColor(randNum);
 	}
