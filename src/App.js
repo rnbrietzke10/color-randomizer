398c47643d96775095ee5ce04d80e634f6c2897b
Rebecca Brietzke <rebeccabrietzke@Rebeccas-MacBook-Pro.local>
2021-08-27 15:52:04 -0700

    added local storage to save state color when page is reloaded

--- src/App.js
+++ src/App.js
@@ -2,31 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 import BgColor from './BgColor';
 
-function randomColor() {
-	let randomColor = Math.floor(Math.random() * 16777215)
-		.toString(16)
-		.toUpperCase();
-	return randomColor;
-}
-
 function App() {
-	const [color, setColor] = useState(randomColor());
-	console.log('top', color);
+	let randColorStored = localStorage.getItem('color');
+	const [color, setColor] = useState(randColorStored);
+	function randomColor() {
+		let randNum = Math.floor(Math.random() * 16777215)
+			.toString(16)
+			.toUpperCase();
+
+		localStorage.setItem('color', randNum);
+		setColor(randNum);
+	}
 	let myStyle = {
 		backgroundColor: '#' + color,
 	};
+
 	return (
 		<div className="container" style={myStyle}>
 			<div className="main">
 				<BgColor color={color} />
-				<div
-					className="btn"
-					onClick={() => {
-						setColor(randomColor());
-						/* Why is this console log 1 color behind */
-						console.log(color);
-					}}
-				>
+				<div className="btn" onClick={randomColor}>
 					Click me
 				</div>
 			</div>
--- src/BgColor.js
+++ src/BgColor.js
@@ -3,7 +3,7 @@ import React from 'react';
 function BgColor(props) {
 	return (
 		<div className="bg-color">
-			<h1>Background Color: {`# ${props.color}`}</h1>
+			<h1>Background Color: {`#${props.color}`}</h1>
 		</div>
 	);
 }
