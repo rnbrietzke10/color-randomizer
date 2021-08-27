1cc9209d306a08863009d56ee57bb0fa11ec1603
Rebecca Brietzke <rebeccabrietzke@Rebeccas-MacBook-Pro.local>
2021-08-26 20:19:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function randomColor() {
+	let randomColor = Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.toUpperCase();
+	return randomColor;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [color, setColor] = useState(randomColor());
+	console.log('top', color);
+	let myStyle = {
+		backgroundColor: '#' + color,
+	};
+	return (
+		<div className="container" style={myStyle}>
+			<div className="main">
+				<div className="bg-color">
+					<h1>Background Color: {`# ${color}`}</h1>
+				</div>
+				<div
+					className="btn"
+					onClick={() => {
+						setColor(randomColor());
+						/* Why is this console log 1 color behind */
+						console.log(color);
+					}}
+				>
+					Click me
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
